760bca916b0e4638f6df9d5f51447ebc3719f472
Ammar Najjar <najjarammar@gmail.com>
2019-03-25 15:36:30 +0100

    test cli entry-points

--- tests/test_automate.py
+++ tests/test_automate.py
@@ -10,28 +10,48 @@ from automate import cli
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def runner():
+    """CliRunner fixture
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    return CliRunner()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
+def test_command_line_cli_interface(runner):
     """Test the CLI."""
-    runner = CliRunner()
     result = runner.invoke(cli.cli)
     assert result.exit_code == 0
     assert 'Console interface for automate' in result.output
+
+
+def test_command_line_interface_help(runner):
     help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
     assert '--help' in help_result.output
     assert 'Show this message and exit.' in help_result.output
+    assert all([entry_point in help_result.output for entry_point in [
+               '-t, --target', 'Target workspace', 'branches', 'dotnet', 'fetch']])
+
+
+def test_fetch_entry_point(runner):
+    help_result = runner.invoke(cli.fetch, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
+
+
+def test_branches_entry_point(runner):
+    help_result = runner.invoke(cli.branches, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
+
+
+def test_dotnet_entry_point(runner):
+    help_result = runner.invoke(cli.dotnet, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
+    assert all([entry_point in help_result.output for entry_point in [
+               '-w, --watch', 'Run in watch mode']])
