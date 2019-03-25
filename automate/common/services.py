8e393bd3ceba985eba286db9fb697468522454c8
Ammar Najjar <najjarammar@gmail.com>
2019-03-25 09:27:14 +0100

    enable running tests in parallel

--- automate/common/services.py
+++ automate/common/services.py
@@ -7,6 +7,7 @@ import shlex
 import subprocess
 from multiprocessing import Process
 from os import path
+from typing import List
 
 from .printing import print_green
 
@@ -19,9 +20,14 @@ def run(project: str, command_type: str, watch_mode: bool):
         command_type {str} -- [run, test, restore, build, clean]
         watch_mode {bool} -- watch code changes
     """
-    command = shlex.split(f'dotnet {command_type} -p {project}')
-    if watch_mode:
-        command = shlex.split(f'dotnet watch -p {project} {command_type}')
+    command: List[str] = []
+    if command_type in ['run']:
+        command = shlex.split(f'dotnet {command_type} -p {project}')
+        if watch_mode:
+            command = shlex.split(f'dotnet watch -p {project} {command_type}')
+    else:
+        command = shlex.split(f'dotnet {command_type} {project}')
+
     process = subprocess.Popen(command)
     process.communicate(input=None)
 
@@ -36,8 +42,18 @@ def start_service(working_directory: str, service_name: str, command: str, watch
         args {[str]} -- projects name (full/partial)
     """
     repo_path = path.join(working_directory, service_name)
-    for project in glob.iglob(f'{repo_path}/**/*.csproj', recursive=True):
-        if not (command == 'test' or 'test' in project.lower()):
-            print_green(service_name)
-            job = Process(target=run, args=(project, command, watch_mode))
-            job.start()
+    test_projects = list(
+        filter(lambda pro: 'test' in pro.lower(), glob.iglob(f'{repo_path}/**/*.csproj', recursive=True)))
+    runnable_projects = list(
+        filter(lambda pro: 'test' not in pro.lower(), glob.iglob(f'{repo_path}/**/*.csproj', recursive=True)))
+
+    exec_pros: List[str] = []
+    if command == 'test':
+        exec_pros = test_projects
+    else:
+        exec_pros = runnable_projects
+
+    for project in exec_pros:
+        print_green(service_name)
+        job = Process(target=run, args=(project, command, watch_mode))
+        job.start()
